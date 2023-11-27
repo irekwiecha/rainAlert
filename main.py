016815a3b5f7bfb8fca8c66b2b9041f81e082d73
irekwiecha <irek88@gmail.com>
2023-11-27 21:49:53 +0100

    feat: get data from endpoint

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import json
+
 import requests
+from icecream import ic
+
+ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 
 # import private data
 with open('priv.json') as f:
     priv = json.load(f)
     APIkey = priv["APIkey"]
+    lat = priv["lat"]
+    lon = priv["lon"]
 
+parameters = {
+    "lat": lat,
+    "lon": lon,
+    "appid": APIkey,
+    "units": "metric"
+}
+
+resp = requests.get(ENDPOINT, params=parameters)
+resp.raise_for_status()
+data = resp.json()
+ic(data)
