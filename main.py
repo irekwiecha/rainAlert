d6de0f6806fe7b5ef6a7f53d89c91d24e4a78165
irekwiecha <irek88@gmail.com>
2023-11-28 21:59:44 +0100

    feat: check weather and send sms alert

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 import json
 
 import requests
-from icecream import ic
+from twilio.rest import Client
 
 ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 
 # import private data
-with open('priv.json') as f:
+with open("priv.json") as f:
     priv = json.load(f)
     APIkey = priv["APIkey"]
     lat = priv["lat"]
     lon = priv["lon"]
+    account_sid = priv["account_sid"]
+    auth_token = priv["auth_token"]
+    twilio_nr = priv["twilio_nr"]
+    my_nr = priv["my_nr"]
 
-parameters = {
-    "lat": lat,
-    "lon": lon,
-    "appid": APIkey,
-    "units": "metric"
-}
+parameters = {"lat": lat, "lon": lon, "appid": APIkey, "units": "metric", "cnt": 4}
 
 resp = requests.get(ENDPOINT, params=parameters)
 resp.raise_for_status()
-data = resp.json()
-ic(data)
+weather_data = resp.json()["list"]
+
+will_rain = False
+for three_hour in weather_data:
+    for data in three_hour["weather"]:
+        if data["id"] < 700:
+            will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        from_=twilio_nr,
+        body="Take an umbrella with you ☂. The precipitation is expected.",
+        to=my_nr,
+    )
